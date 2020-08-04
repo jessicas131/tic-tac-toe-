a32dba308b8641fef607743de91446e0e2d2b6fb
jstevens <jessica.stevens131@gmail.com>
2020-08-03 21:52:03 -0500

    finish notes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const MAXGUESS = 9;
 
-//winning combinations
+//possible winning combinations
 const WINARR = [
   [0, 1, 2],
   [3, 4, 5],
@@ -15,47 +15,51 @@ const WINARR = [
 ];
 
 /*--------------------State Variables--------------------*/
-//squares are undefined to start game then defined later by button click either x or o
+//squares are undefined to start game then defined later by button click... either x or o
 let playerChoices; //player one = 1  player 2= -1
-let turn;
+let turn; //determined by positive or negative to switch back and forth
 /*--------------------Cached Element Refrences--------------------*/
-//html "square" elements
-//let squareEl = document.getElementsByClassName(".gameBoard");
-
-//const button = document.getElementsByTagName(button);
+let gameBoardEl = document.querySelector(".gameBoard");
 
 /*--------------------Event Listeners--------------------*/
+//need to be able to click whole game board
 document.querySelector(".gameBoard").addEventListener("click", handleClick);
+//when the restart button is hit it goes back to initialize function
 document.querySelector("button").addEventListener("click", init);
 /*--------------------Functions--------------------*/
 
 //event handler function
-
+//the event parameter is refering to the event listener
+//let index we made a variable for the target()
+//playerChoices[0] = turn % 2 ? "O" : "X";   changing the value in the array
+//then call the render function to "mark" x or o
+//once a winner is found invoke the renderMessage function
+//if we reach max guesses without a winner then disable the clicks and display message
 function handleClick(evt) {
   let index = evt.target.id;
+  if (playerChoices[index] || checkWin()) return;
   playerChoices[index] = turn % 2 ? "O" : "X";
   render(evt.target);
-
-  console.log(playerChoices);
   if (checkWin()) {
-    document
-      .querySelector(".gameBoard")
-      .removeEventListener("click", handleClick);
     renderMessage();
   } else if (turn + 1 === MAXGUESS) {
-    document
-      .querySelector(".gameBoard")
-      .removeEventListener("click", handleClick);
+    gameBoardEl.removeEventListener("click", handleClick);
     document.getElementById("msg").textContent = "Nobody Wins";
   }
   turn++;
 }
 
+//this sets all of the state variables
+//and sets the message to null
+//the init starts the game and RESTARTS the game
+//playerChoices starts at null so we can click and fill it
 function init() {
   //initialize all state variables
   playerChoices = [null, null, null, null, null, null, null, null, null];
+  //for when it restarts
   turn = 0;
   document.querySelectorAll(".box").forEach(function (el) {
+    //this is fixing my weird css
     el.textContent = "x";
     el.style.color = "#101357";
   });
@@ -65,6 +69,9 @@ function init() {
 
 init();
 
+//compairing the winning combos array to the values that have been entered into the player choices array
+//array some checks to see if a match is immediately true
+//the fourth is to make sure the three indexes that equal eachother arent null
 function checkWin() {
   //check if there is a winner
   return WINARR.some(function (arr) {
@@ -76,11 +83,13 @@ function checkWin() {
   });
 }
 
+//this is what is putting my x's and o's on the board
 function render(element) {
   element.textContent = turn % 2 ? "O" : "X";
   element.style.color = "white";
 }
 
+//when this message is called
 function renderMessage() {
   document.getElementById("msg").textContent = `Play ${
     turn % 2 ? "O" : "X"
